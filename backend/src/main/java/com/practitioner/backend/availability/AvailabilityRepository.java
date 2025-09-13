0af69e32147a20a80974f2ecdedfbd97ed0176df
a2mar <a2mar@protonmail.ch>
2025-09-13 19:25:27 +0200

    feat(availability): add availability repository

---
+++ backend/src/main/java/com/practitioner/backend/availability/AvailabilityRepository.java
@@ -0,0 +1,6 @@
+package com.practitioner.backend.availability;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvailabilityRepository extends JpaRepository<Availability, Long> {
+}
