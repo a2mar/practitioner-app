aad3dfe7088bb3a3ef0837fe7a3f681f4e0b353b
a2mar <a2mar@protonmail.ch>
2025-09-13 19:14:58 +0200

    feat(practitioner): add practitioner entity

---
+++ backend/src/main/java/com/practitioner/backend/practitioner/Practitioner.java
@@ -0,0 +1,25 @@
+package com.practitioner.backend.practitioner;
+
+import com.practitioner.backend.user.User;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "practitioners")
+public class Practitioner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(columnDefinition = "TEXT")
+    private String bio;
+
+    @Column(columnDefinition = "TEXT")
+    private String specialties;
+
+    // getters & setters
+}
