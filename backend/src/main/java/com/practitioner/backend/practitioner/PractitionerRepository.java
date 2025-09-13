ea8cc5e6747aeef0d6df40816e2eb3393ba008e9
a2mar <a2mar@protonmail.ch>
2025-09-13 19:23:47 +0200

    feat(practitioner): add practitioner repository

---
+++ backend/src/main/java/com/practitioner/backend/practitioner/PractitionerRepository.java
@@ -0,0 +1,6 @@
+package com.practitioner.backend.practitioner;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PractitionerRepository extends JpaRepository<Practitioner, Long> {
+}
