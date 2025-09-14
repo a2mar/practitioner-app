f82a8979e4aff1893188c827cc15b64662406d43
a2mar <a2mar@protonmail.ch>
2025-09-14 11:30:05 +0200

    feat(practitioner): add getter and setter

--- backend/src/main/java/com/practitioner/backend/practitioner/Practitioner.java
+++ backend/src/main/java/com/practitioner/backend/practitioner/Practitioner.java
@@ -2,7 +2,11 @@ package com.practitioner.backend.practitioner;
 
 import com.practitioner.backend.user.User;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "practitioners")
 public class Practitioner {
@@ -20,6 +24,4 @@ public class Practitioner {
 
     @Column(columnDefinition = "TEXT")
     private String specialties;
-
-    // getters & setters
 }
