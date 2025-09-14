3464a710bededc90f74b3be5d63b4c8a5b4bfc09
a2mar <a2mar@protonmail.ch>
2025-09-14 12:59:49 +0200

    test: add abstreact integration test class for entities

---
+++ backend/src/test/java/com/practitioner/backend/AbstractIntegrationTest.java
@@ -0,0 +1,18 @@
+package com.practitioner.backend;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+@SpringBootTest
+public abstract class AbstractIntegrationTest {
+
+    private static final PostgresTestContainer POSTGRES = PostgresTestContainer.getInstance();
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
+}
