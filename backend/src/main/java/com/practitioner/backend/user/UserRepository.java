6faca3851fe04a3458c5b9b9631bbf4fbdaa747d
a2mar <a2mar@protonmail.ch>
2025-09-14 12:13:33 +0200

    docs: improve inline comment on query derivation

--- backend/src/main/java/com/practitioner/backend/user/UserRepository.java
+++ backend/src/main/java/com/practitioner/backend/user/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // duery derivation: this method gets implemented at runtime using the 'findBy' keyword and 'Email'
+    // query derivation: this method gets implemented at runtime using the 'findBy' keyword and 'Email'
     Optional<User> findByEmail(String email); 
 }
